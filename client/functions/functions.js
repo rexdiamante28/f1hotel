a2e49acf9a40a1ce0da72230ce99b893b0147b37
nico1214 <nicolo_henrietta@yahoo.com>
2015-11-09 14:17:45 +0800

    updated source

--- client/functions/functions.js
+++ client/functions/functions.js
@@ -1,3 +1,8 @@
 goHome = function gohome(){
     Router.go('/');
 }
+
+hideNav = function hidenav(){
+        //$('.btn-navbar').click();
+        $('.navbar-toggle').click()
+}
--- route.js
+++ route.js
@@ -17,3 +17,54 @@ Router.route('/rooms/deluxe', function () {
   this.render('deluxe');
   this.layout('layout');
 });
+
+Router.route('/rooms/city', function () {
+  this.render('city');
+  this.layout('layout');
+});
+
+Router.route('/rooms/fort', function () {
+  this.render('fort');
+  this.layout('layout');
+});
+
+
+Router.route('/dinning', function () {
+  this.render('dinning');
+  this.layout('layout');
+});
+
+Router.route('/celebrate', function () {
+  this.render('celebrate');
+  this.layout('layout');
+});
+
+Router.route('/wow', function () {
+  this.render('wow');
+  this.layout('layout');
+});
+
+Router.route('/news', function () {
+  this.render('news');
+  this.layout('layout');
+});
+
+Router.route('/contact', function () {
+  this.render('contact');
+  this.layout('layout');
+});
+
+Router.route('/news1', function () {
+  this.render('news1');
+  this.layout('layout');
+});
+
+Router.route('/news2', function () {
+  this.render('news2');
+  this.layout('layout');
+});
+
+Router.route('/reserve', function () {
+  this.render('reserve');
+  this.layout('layout');
+});
